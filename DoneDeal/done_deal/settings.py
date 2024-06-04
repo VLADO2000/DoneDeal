8f93ee2b6cb9624748c686f5ec810c8d45be657a
WLOT <vladlotov@ukr.net>
2024-06-04 17:44:36 +0200

    environment shift secret-key

--- DoneDeal/done_deal/settings.py
+++ DoneDeal/done_deal/settings.py
@@ -13,6 +13,8 @@ import os
 
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0=+*k@@9^1!=o(pwg6pr*2)5*0i+=y!2+uch8u_g%)=%j($s3("
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
