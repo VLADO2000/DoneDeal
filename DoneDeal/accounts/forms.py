f69e0ccb150737c5f58b8cacfe27531b60880b1d
WLOT <vladlotov@ukr.net>
2024-06-07 18:02:07 +0200

    resolved issues with role and brand_name

--- DoneDeal/accounts/forms.py
+++ DoneDeal/accounts/forms.py
@@ -7,7 +7,7 @@ class CustomAccountForm(SignupForm):
     brand_name = forms.CharField(max_length=255)
     role = forms.ChoiceField(choices=[(0, 'individual'),(1, 'company'),])
     
-    def safe(self,request):
+    def save(self, request):
         user = super(CustomAccountForm, self).save(request)
         user.brand_name = self.cleaned_data['brand_name']
         user.role = self.cleaned_data['role']
